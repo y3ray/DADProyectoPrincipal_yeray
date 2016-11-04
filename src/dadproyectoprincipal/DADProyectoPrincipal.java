d6ce6259e67930fe4977a39eb0f2ad7599ded06d
Yeray Tejera <Yeray@LAPTOP-VOIBHU16>
2016-11-04 12:40:14 +0000

    Primer commit

---
+++ src/dadproyectoprincipal/DADProyectoPrincipal.java
@@ -0,0 +1,13 @@
+
+package dadproyectoprincipal;
+
+
+public class DADProyectoPrincipal {
+
+   
+    public static void main(String[] args) {
+        frmPrincipal vista = new frmPrincipal();
+        vista.setVisible(true);
+    }
+    
+}
---
+++ src/dadproyectoprincipal/DTO_Alumnos.java
@@ -0,0 +1,56 @@
+
+package dadproyectoprincipal;
+
+public class DTO_Alumnos {
+    private int registro;
+    private String dni;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    
+    public DTO_Alumnos(){
+        
+    }
+
+    public int getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(int registro) {
+        this.registro = registro;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+    
+    
+}
